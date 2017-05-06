7926aff761cab02444acae1b3653c1722bb92004
JD <blackavar.erinsward@gmail.com>
2017-05-06 18:33:42 +0100

    Basic test notes

---
+++ tests/Test+Scratchpad.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+# Import the function that's going to output the password
+# Tests:
+# Dictionary file incorrect format handled (we want newline-delimited text file with one word per line)
+# Dictonary file non-existence handled
+# 4-word password; check output is 4 words with spaces, each word is >= 5 characters long, UTF-8 format
+# 5-word password; check output is 5 words with spaces, each word is >= 5 characters long, UTF-8 format
+# 6-word password; check output is 6 words with spaces, each word is >= 5 characters long, UTF-8 format
+
