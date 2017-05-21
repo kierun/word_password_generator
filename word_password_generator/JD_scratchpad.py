75e6429c1377cbd863ee14324eb20827bf468899
Jonathan Deighton <blackavar.erinsward@gmail.com>
2017-05-21 15:47:03 +0100

    Code split into functions and main() created

--- word_password_generator/JD_scratchpad.py
+++ word_password_generator/JD_scratchpad.py
@@ -1,61 +1,91 @@
-# Add user input here for file path & name, also for whether they want a 4, 5 or 6 word password
-# Check for 'file exists', can we also check for valid format (UTF-8)?
-
-# import magic # So, I tried to use this but for some reason it doesn't want to import...
 from pathlib import Path
-import sys
 from random import randrange
 
-print('How many words would you like in your password (4-6)?')
-number_of_words = input()
-while(int(number_of_words) < 4) or (int(number_of_words) > 6):
-    print('That value is outside the accepted range (4-6), please try again:')
+def AskNumberOfWords():
+    # Ask the user for a number in the range 4 to 6 to use later for number of words selected
+    print('How many words would you like in your password (4-6)?')
     number_of_words = input()
+    # Make sure we do only take a number in the range 4 to 6!
+    while(int(number_of_words) < 4) or (int(number_of_words) > 6):
+        print('That value is outside the accepted range (4-6), please try again:')
+        number_of_words = input()
+    return number_of_words
 
-print('Please enter the path to the word list file:')
-file_path_input = input()
-file_path = Path(file_path_input)
-if not file_path.is_file():
-    print('File not present at specified location, please run again...')
-    exit()
+def AskForFileLocation():
+    # Ask the user for the path to the word list file - wanted to do more here, but analysing
+    # the format of text files seems tough and I wanted to get this done
+    print('Please enter the path to the word list file:')
+    file_path_input = input()
+    file_path = Path(file_path_input)
+    # We can at least check the file location is legitimate
+    if not file_path.is_file():
+        print('File not present at specified location, please run again...')
+        file_path = AskForFileLocation()
+    return file_path
 
-    
 
-# Apparently this should open the file, stream out the contents as asked for then closes the file?
-# Tried just loading the whole file into memory, but it crapped out, so we'll do this
-with open(file_path) as datafile:
-    words = (word.rstrip('\r\n') for word in datafile)
-    desiredWords = list()
-    for word in words:
-        if(len(word) <=6 and len(word) >= 4):
-            # Try adding the filtered word list to an array of strings?
-            desiredWords.append(word)
-    # Here, maybe get the string-array length and feed that in as the top end of a random number generator
+def SelectWordsFromList(word_list, number_of_words):
+    # Here, get the string-array length and feed that in as the top end of a random number generator
     # Use user-input for number of words desired to cycle through the array and pick random words out
-    list_length = len(desiredWords)
+    list_length = len(word_list)
     output_list = list()
     for count in range(0, int(number_of_words)):
         selection = randrange(0, list_length, 1)
-        new_word = desiredWords[selection]
+        new_word = word_list[selection]
         # print('Word number: ' + str(selection +1) + ', ' + new_word)
         while(new_word in output_list):
             # print('DUPLICATE!!!')
             selection = randrange(0, list_length, 1)
-            new_word = desiredWords[selection]
+            new_word = word_list[selection]
             # print('Word number: ' + str(selection +1) + ', ' + new_word)
         output_list.append(new_word)
         count += 1
-    for output_words in output_list:
-        print(output_words, end=',')
-    print('\nAre these words acceptable (Y/N)?')
-    acceptance = input().upper()
-    while (acceptance != 'Y') and (acceptance !='N'):
-        print('Y or N, please!')
+    return output_list
+
+
+def main():
+    # Ask for the desired number of words
+    number_of_words = AskNumberOfWords()
+    # Ask for a word list file
+    file_path = AskForFileLocation()
+
+    # use 'with' to open and process the file contents so that when completed the resources are freed
+    with open(file_path) as datafile:
+        words = (word.rstrip('\r\n') for word in datafile)
+        desired_words = list()
+        # Loop down the list of words and store the desirable ones
+        for word in words:
+            if(len(word) <=6 and len(word) >= 4):
+                # Try adding the filtered word list to an array of strings?
+                desired_words.append(word)
+        # Select a unique set of words from the stored list, number defined earlier by the user
+        output_list = SelectWordsFromList(desired_words, number_of_words)
+        # Output the word selection to the console and see if the user likes them
+        for output_words in output_list:
+            print(output_words, end=',')
+        print('\nAre these words acceptable (Y/N)?')
         acceptance = input().upper()
-    if(acceptance == 'Y'):
+        while (acceptance != 'Y') and (acceptance !='N'):
+            print('Y or N, please!')
+            acceptance = input().upper()
+        # As long as the user doesn't like the selection, make another one
+        while(acceptance == 'N'):
+            print('Sorry about that, let\'s try again!')
+            output_list = SelectWordsFromList(desired_words, number_of_words)
+            for output_words in output_list:
+                print(output_words, end=',')
+            print('\nAre these words acceptable (Y/N)?')
+            acceptance = input().upper()
+            while (acceptance != 'Y') and (acceptance !='N'):
+                print('Y or N, please!')
+                acceptance = input().upper()
         print('Happy password usage!')
-    else:
-        print('Sorry about that - please run again...')
+
+   
+if __name__ == "__main__":
+    main()
+
+
     
 
    
